3535669fecc653113911b91ca5b8f256c426af4c
Tiago <tnhhilario@gmail.com>
2022-11-02 01:15:00 -0300

    resp questao 3

---
+++ src/main/java/com/tiago/desafioTecnico/PalavraOutraPalavra.java
@@ -0,0 +1,15 @@
+package com.tiago.desafioTecnico;
+
+public class PalavraOutraPalavra {
+    public static void main(String[] args) {
+        String palavra = "a";
+        String outraPalavra = "a";
+
+        if (palavra == outraPalavra) {
+            System.out.println("São iguais");
+        }else{
+            System.out.println("São diferentes");
+        }
+
+    }
+}
