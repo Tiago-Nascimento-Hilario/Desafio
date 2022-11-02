375299cc861e85319992e205534d075994126003
Tiago <tnhhilario@gmail.com>
2022-11-02 00:53:20 -0300

    resp questão 2

---
+++ src/main/java/com/tiago/desafioTecnico/Veiculo.java
@@ -0,0 +1,45 @@
+package com.tiago.desafioTecnico;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Veiculo {
+    
+    @Id
+    private String placa;
+    private String carro;
+    private String cor;
+    private String dataDeFabricacao;
+
+
+    public String getPlaca() {
+        return placa;
+    }
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+    public String getCarro() {
+        return carro;
+    }
+    public void setCarro(String carro) {
+        this.carro = carro;
+    }
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    public String getDataDeFabricacao() {
+        return dataDeFabricacao;
+    }
+    public void setDataDeFabricacao(String dataDeFabricacao) {
+        this.dataDeFabricacao = dataDeFabricacao;
+    }
+
+    
+
+}
