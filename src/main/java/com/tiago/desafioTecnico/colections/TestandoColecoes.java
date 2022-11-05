e450bc6f7fc24abf81074fa378893e2ef70b949f
Tiago <tnhhilario@gmail.com>
2022-11-04 22:18:54 -0300

    resp 7 - HashSet

--- src/main/java/com/tiago/desafioTecnico/colections/TestandoColecoes.java
+++ src/main/java/com/tiago/desafioTecnico/colections/TestandoColecoes.java
@@ -1,19 +1,22 @@
 package com.tiago.desafiotecnico.colections;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 
 public class TestandoColecoes {
     public static void main(String[] args) {
 
-        /* Colection arrayList - Função criar uma lista dinâmica
+        /* Colection hashSet - Função criar  um conjunto, não permitir duplicidade 
          * Nesse exemplo criei uma classe Empresa com os atributos Nome do funcionário e matrícula, criei um construtor e os metodos get e set.
-         * utilizei a colection arrayList para criar uma lista dinâmica visto outros funcionários poderam ser adicionados
-         * Fiz o foreach para listar os dados dos funcionários que foram incluídos na lista.
+         * utilizei a colection hashSet para criar um conjunto assim não ocorrer duplicidade visto que outros funcionários poderam ser adicionados
+         * Fiz o foreach para listar os dados dos funcionários que foram incluídos no conjunto.
          */
-        ArrayList<Empresa> listaDEFuncionarios = new ArrayList<Empresa>();
+        HashSet<Empresa> listaDEFuncionarios = new HashSet<Empresa>();
 
         listaDEFuncionarios.add(new Empresa("Tiago Nascimento Hilario", 1234));
         listaDEFuncionarios.add(new Empresa("Arthur Teixeira Hilario",2345));
+        //Teste - Duplicidade
+        listaDEFuncionarios.add(new Empresa("Tiago Nascimento Hilario", 1234));
+
 
         for (Empresa listarEmpresa : listaDEFuncionarios) {
              System.out.println("Funcionário: "+listarEmpresa.getNomeDoFuncionario()+"\n"+"Matrícula: "+listarEmpresa.getMatriculaDoFuncionario());
